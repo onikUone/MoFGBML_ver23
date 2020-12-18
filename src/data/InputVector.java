1d9dc752fc4d13548537d8d4a35101aa9a707606
omozaki <yuichi.omozaki@ci.cs.osakafu-u.ac.jp>
2020-12-19 01:40:43 +0900

    create InputVector.java

---
+++ src/data/InputVector.java
@@ -0,0 +1,43 @@
+package data;
+
+import java.util.Arrays;
+
+public class InputVector {
+	// ************************************************************
+	// Fields
+
+	// Input vector
+	double[] inputVector;
+
+	// ************************************************************
+	// Constructor
+
+	public InputVector(int dimension) {
+		this.inputVector = new double[dimension];
+	}
+
+	// ************************************************************
+	// Methods
+
+	/**
+	 *
+	 */
+	public double getDimValue(int index) {
+		return this.inputVector[index];
+	}
+
+	/**
+	 *
+	 */
+	public double[] getVector() {
+		return this.inputVector;
+	}
+
+	/**
+	 *
+	 */
+	public void setVector(double[] inputVector) {
+		this.inputVector = Arrays.copyOf(inputVector, inputVector.length);
+	}
+
+}
