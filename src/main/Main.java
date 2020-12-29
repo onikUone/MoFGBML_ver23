5adff73d98e817bccf597fe7f35d73b9206ae49f
omozaki <yuichi.omozaki@ci.cs.osakafu-u.ac.jp>
2020-12-29 21:35:00 +0900

    test Build package

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,12 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Maven!");
 
+
+		System.out.println(Consts.ANTECEDENT_LEN);
+		String source = args[0];
+		Consts.setConsts(source);
+		System.out.println(Consts.ANTECEDENT_LEN);
+
 	}
 
 }
